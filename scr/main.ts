761e24347c0d457c6591d75bfd6eea3063a71fc3
dtfigueiredo <dt.figueiredo90@gmail.com>
2021-11-13 16:27:23 -0300

    consts para maiusculas + addEventListener keypress

--- scr/main.ts
+++ scr/main.ts
@@ -20,24 +20,27 @@ const MAIN = {
 
   //função que é responsável por gerar as interações de eventos
   bindEvents: function () {
-    const self = this;
+    const SELF = this;
 
     this.$checkButtons.forEach(function (button) {
-      button.addEventListener("click", self.Events.checkButtonClick);
+      button.addEventListener("click", SELF.Events.checkButtonClick);
     });
 
-    this.$inputTask.onkeypress = self.Events.inputTaskEnter.bind(this);
+    this.$inputTask.addEventListener(
+      "keypress",
+      SELF.Events.inputTaskEnter.bind(this)
+    );
 
     this.$removeButtons.forEach(function (button) {
-      button.addEventListener("click", self.Events.removeTaskClick);
+      button.addEventListener("click", SELF.Events.removeTaskClick);
     });
   },
 
   //função responsável por buscar os valores salvos no LocalStorage, a partir do "inputTaskEnter" linha 54
   getLocalStorage: function () {
-    const tasksStoraged = localStorage.getItem("tasks");
+    const TASKSSTORAGED = localStorage.getItem("tasks");
 
-    this.tasksList = JSON.parse(tasksStoraged);
+    this.tasksList = JSON.parse(TASKSSTORAGED);
   },
 
   //função responsável por criar a lista de tarefas a partir dos dados inseridos no array 'tasksList' (linha 02),que foi gerado através do "getLocalStorage" linha38
@@ -63,19 +66,19 @@ const MAIN = {
   //seção responsável por armazenar todas as funções de eventos isolados
   Events: {
     checkButtonClick: function (e) {
-      const li = e.target.parentNode;
-      const isDone = li.classList.contains("done");
+      const LI = e.target.parentNode;
+      const ISDONE = LI.classList.contains("done");
 
       //transforma o botão em toogle on-off pra classe 'done' => caso exista ele remove, caso não exista ele adiciona
-      isDone ? li.classList.remove("done") : li.classList.add("done");
+      ISDONE ? LI.classList.remove("done") : LI.classList.add("done");
     },
 
     inputTaskEnter: function (e) {
-      const key = e.key;
+      const KEY = e.key;
       let input = e.target;
       let task = input.value;
 
-      if (key === "Enter" && task.length > 0) {
+      if (KEY === "Enter" && task.length > 0) {
         this.$spanError.innerText = "";
 
         this.$list.innerHTML += `
@@ -98,19 +101,20 @@ const MAIN = {
         localStorage.setItem("tasks", JSON.stringify(TASKOBJ));
       }
 
-      if (!task.length && key === "Enter") {
+      if (!task.length && KEY === "Enter") {
         this.$spanError.innerHTML = `<i class="fas fa-exclamation-circle"></i> Preencha a próxima tarefa corretamente.`;
       }
     },
 
     removeTaskClick: function (e) {
-      console.log(e.target.parentElement.parentElement);
-      const li = e.target.parentElement.parentElement;
+      const LI = e.target.parentElement.parentElement;
 
-      li.classList.add("removed");
+      console.log(LI);
+
+      LI.classList.add("removed");
 
       setTimeout(() => {
-        li.classList.add("hidden");
+        LI.classList.add("hidden");
       }, 500);
     },
   },
